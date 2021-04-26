536fb2754f5c03e4b7951b020e0739fa77aacc33
yaseru2003@gmail.com <yaseru2003@gmail.com>
2021-04-25 23:29:26 -0400

    login,logout,signup, and login_status

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,7 +1,35 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from accounts.api.serializers import UserSerializer, SignupSerializer, LoginSerializer
+from django.contrib.auth import (
+    authenticate as django_authenticate,
+    login as django_login,
+    logout as django_logout,
+)
 
-from accounts.api.serializers import UserSerializer
+"""
+dir(request)
+
+['DATA', 'FILES', 'POST', 'QUERY_PARAMS', '__class__', '__delattr__',
+ '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', 
+ '__getattr__', '__getattribute__', '__gt__', '__hash__', '__init__', 
+ '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', 
+ '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', 
+ '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_auth', 
+ '_authenticate', '_authenticator', '_content_type', '_data', 
+ '_default_negotiator', '_files', '_full_data', '_load_data_and_files', 
+ '_load_stream', '_not_authenticated', '_parse', '_request', '_stream', 
+ '_supports_form_parsing', '_user', 'accepted_media_type', 'accepted_renderer', 
+ 'auth', 'authenticators', 'content_type', 'data', 'force_plaintext_errors', 
+ 'negotiator', 'parser_context', 'parsers', 'query_params', 'stream', 
+ 'successful_authenticator', 'user', 'version', 'versioning_scheme']
+
+
+
+"""
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -11,3 +39,65 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class AccountViewSet(viewsets.ModelViewSet):
+    permission_classes = (AllowAny,)
+    serializer_class = SignupSerializer
+
+    @action(methods=['POST'], detail=False)
+    def signup(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': 'Please check input',
+                'errors': serializer.errors,
+            }, status=400)
+        user = serializer.save()
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(user).data,
+        })
+
+    @action(methods=['GET'], detail=False)
+    def login_status(self, request):
+        print(request)
+        print(request.user)
+        print(request.data)
+        data = {'has_logged_in': request.user.is_authenticated}
+        if request.user.is_authenticated:
+            data['user'] = UserSerializer(request.user).data
+        return Response(data)
+
+    @action(methods=['POST'], detail=False)
+    def logout(self, request):
+        django_logout(request)
+        return Response({'success': True})
+
+    @action(methods=['POST'], detail=False)
+    def login(self, request):
+        # to validate user data
+        print(dir(request))
+        serializer = LoginSerializer(data=request.data)  # request.query_params for GET
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input.",
+                "errors": serializer.errors,
+            }, status=400)
+        username = serializer.validated_data['username']
+        password = serializer.validated_data['password']
+        # check user's authentication
+        user = django_authenticate(username=username,password=password)
+        if not user or user.is_anonymous:
+            return Response({
+                "success": False,
+                "message": "username and password does not match",
+            }, status=400)
+        django_login(request, user)
+        return Response({
+            "success": True,
+            "user": UserSerializer(instance=user).data
+        }, status=200)
