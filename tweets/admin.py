c8c353c41c9bc1eef69fe1016d3eb2cfb88b03f6
yaseru2003@gmail.com <yaseru2003@gmail.com>
2021-05-08 23:11:13 -0400

    added admin

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from tweets.models import Tweet
+
+
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_at'
+    list_display = (
+        'created_at',
+        'user',
+        'content',
+    )
